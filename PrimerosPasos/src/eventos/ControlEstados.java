b1b788c2a994ac29942983b7a7e3a47188e67902
luigi-coder <luigicoder28@gmail.com>
2022-04-13 13:43:53 -0300

    Control de estados de la ventana

---
+++ PrimerosPasos/src/eventos/ControlEstados.java
@@ -0,0 +1,70 @@
+package eventos;
+
+
+import java.awt.event.*;
+
+import java.awt.Frame;
+import javax.swing.JFrame;
+
+public class ControlEstados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MarcoControlEstado mimarco = new MarcoControlEstado();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
+
+class MarcoControlEstado extends JFrame{
+
+	public MarcoControlEstado() {
+		
+		setVisible(true);
+		
+		setBounds(300,300,500,350);
+		
+		// addWindowStateListener estar a la escucha 
+		// del nuevo estado (new MensajeEstado())
+		addWindowStateListener(new MensajeEstado());
+	}
+
+}
+
+
+class MensajeEstado implements WindowStateListener {
+
+	@Override
+	public void windowStateChanged(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+		if(e.getNewState()==Frame.MAXIMIZED_BOTH) {
+			
+			System.out.println("La ventana esta a pantalla completa");
+			
+		}else if(e.getNewState()==Frame.NORMAL) {
+			
+			System.out.println("La ventana esta normal");
+			
+		}else if (e.getNewState()==Frame.ICONIFIED) {
+			
+			System.out.println("La ventana esta minimizada");
+		}
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
