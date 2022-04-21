93e97df0e6fea6ae7822fedeb26d2f792ed8b52f
luigi-coder <luigicoder28@gmail.com>
2022-04-21 13:09:03 -0300

    ArrayList - Iterator

--- PrimerosPasos/src/programacion_generica/MainIterador.java
+++ PrimerosPasos/src/programacion_generica/MainIterador.java
@@ -1,6 +1,7 @@
 package programacion_generica;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MainIterador {
 
@@ -15,6 +16,20 @@ public class MainIterador {
 	    listaPersonas.add(new Persona(2,"Lucy",12));
 	    listaPersonas.add(new Persona(3,"Guillermo",54));
 	    listaPersonas.add(new Persona(4,"Luisina",29));
+	    
+	    Iterator<Persona> iterator = listaPersonas.iterator();
+	    
+	    while (iterator.hasNext()) {
+			
+	    	System.out.println(iterator.next().getNombre());
+			
+		}
+	    
+	    for (Iterator<Persona> iterator2 = listaPersonas.iterator(); iterator2.hasNext();) {
+			Persona persona = (Persona) iterator2.next();
+			System.out.println(persona.getNombre());
+			
+		}
 	    
 	}
 
