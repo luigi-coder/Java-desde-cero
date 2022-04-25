70916fd6f54727062fb0b98f7ac071b2e4d196e6
luigi-coder <luigicoder28@gmail.com>
2022-04-25 11:45:37 -0300

    Excepciones

---
+++ PrimerosPasos/src/programacion_generica/Excepciones.java
@@ -0,0 +1,20 @@
+package programacion_generica;
+
+public class Excepciones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			
+			double division = 35/0;
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			
+			System.out.println("Fallo");
+		}
+
+	}
+
+}
